8365c3b6b7efe505fc234a3e4f86673b8d423c23
Will Riesen <42421306+willriesen@users.noreply.github.com>
2019-02-21 13:07:58 -0600

    Finished ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,8 +4,8 @@ class ToDo extends Component {
    render() {
      return (
        <li>
-         <input type="checkbox" checked={ this.props.isCompleted } />
-         <span>{ this.props.description }</span>
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <span>{this.props.description}</span>
        </li>
      );
    }
