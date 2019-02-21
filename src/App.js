3c7aeaf93298d649d66b61c54ec65840c7aff0d6
Will Riesen <42421306+willriesen@users.noreply.github.com>
2019-02-21 08:04:20 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
  
  import './App.css';
+ import ToDo from './components/ToDo.js';
 
- class App extends Component {
+class App extends Component {
    render() {
      return (
        <div className="App">
-         
+          <ul>
+          <ToDo />
+          <ToDo />
+        </ul> 
        </div>
      );
    }
